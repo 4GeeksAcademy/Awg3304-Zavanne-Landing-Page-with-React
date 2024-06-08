b609173600df0e9a3309e589b860e373daf92653
Awg3304 <166061446+Awg3304@users.noreply.github.com>
2024-06-08 19:35:14 +0000

    Finished implementing the Navbar & Jumbotron.

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Jumbotron = () => {
   return (
-    <div>Jumbotron</div>
+      <div className="jumbotron shadow-lg p-3 mb-5 bg-light m-5 rounded position-absolute top-5">
+  <div className="container">
+    <h1 className="display-5 position-relative top-0">A Warm Welcome!</h1>
+    <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pellentesque aliquet nulla in hendrerit. Vestibulum quis elit elit. Maecenas ultrices sit amet elit non tincidunt. Mauris in nisl convallis, accumsan tellus in, condimentum felis. Maecenas eget enim ac odio pretium molestie. Nunc non dolor quam.</p>
+    <button type="button" class="btn btn-dark">Call To Action!</button>
+  </div>
+</div>
   )
 }
 
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -2,27 +2,29 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
     <div className="container-fluid">
-      <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
+      <a className="navbar-brand" href="#">Start Bootstrap</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="col position-absolute top-1 end-0">
         <ul className="navbar-nav">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">About</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
+            <a className="nav-link" href="#">Services</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            <a className="nav-link" href="#">Contact Us</a>
           </li>
         </ul>
+        </div>
       </div>
     </div>
   </nav>
